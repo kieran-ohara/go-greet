c37f0cfaae7bc2518a851789046167082060e882
Kieran Bamforth <hello@kieranbamforth.me>
2022-11-13 22:32:51 +0000

    throw an error if name is empty.

--- greetings.go
+++ greetings.go
@@ -1,10 +1,15 @@
 package gogreet
 
 import "fmt"
+import "errors"
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
-    // Return a greeting that embeds the name in a message.
-    message := fmt.Sprintf("Hi, %v. Welcome!", name)
-    return message
+func Hello(name string) (string, error) {
+	// Return a greeting that embeds the name in a message.
+	if name == "" {
+		return "", errors.New("empty name")
+
+	}
+	message := fmt.Sprintf("Hi, %v. Welcome, LOL!", name)
+	return message, nil
 }
