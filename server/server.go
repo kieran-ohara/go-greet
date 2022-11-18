23ad3096e9aa25c882c493ebdc716aaffb8a1699
Kieran Bamforth <hello@kieranbamforth.me>
2022-11-18 15:11:24 +0000

    create the grpc sewrver.

--- server/server.go
+++ server/server.go
@@ -1,8 +1,10 @@
 package server
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net"
+	"net/http"
+  "google.golang.org/grpc"
 )
 
 func Serve() {
@@ -10,4 +12,14 @@ func Serve() {
   if err != nil {
     fmt.Println( "there was an error" )
   }
+}
+
+func RpcServe() {
+  lis, err := net.Listen("tcp", fmt.Sprintf("localhost:1234"))
+  if err != nil {
+    fmt.Sprintln("there wasa grpc error")
+  }
+  var opts []grpc.ServerOption
+  grpcServer := grpc.NewServer(opts...)
+  grpcServer.Serve(lis)
 }
